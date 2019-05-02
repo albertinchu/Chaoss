34150b30ec3c1ea6b4d063d8efde8714c91532d1
albertinchu Martin Palomino <albertinchu@hotmail.com>
2019-05-02 19:59:40 +0200

    hola

---
+++ Chaoss/Chaoss.cs
@@ -0,0 +1,45 @@
+﻿using Smod2;
+using Smod2.Attributes;
+using scp4aiur;
+namespace Chaoss
+{
+    [PluginDetails(
+        author = "Albertinchu",
+        name = "Chaoss",
+        description = "attention all facility personnel the chaos insurgency has broken the security of the facility ",
+        id = "albertinchu.chaoss",
+        version = "1.0.0",
+        SmodMajor = 3,
+        SmodMinor = 0,
+        SmodRevision = 0
+        )]
+    public class Chaoss : Plugin
+    {
+
+        public override void OnDisable()
+        {
+            this.Info("Chaoss - Desactivado");
+        }
+
+        public override void OnEnable()
+        {
+            Info("Chaoss - Activado.");
+        }
+
+        public override void Register()
+        {
+           
+            Timing.Init(this);
+            this.AddEventHandlers(new Event(this));
+
+        }
+        public void RefreshConfig()
+        {
+
+
+        }
+    }
+
+}
+
+
---
+++ Chaoss/Event.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Smod2;
+using Smod2.EventHandlers;
+using Smod2.Events;
+using scp4aiur;
+using Smod2.EventSystem.Events;
+
+namespace Chaoss
+{
+    partial class Event : IEventHandlerTeamRespawn
+    {
+        private Chaoss plugin;
+
+        public Event(Chaoss plugin)
+        {
+            this.plugin = plugin;
+        }
+
+
+
+        public void OnTeamRespawn(TeamRespawnEvent ev)
+        {
+            if (ev.SpawnChaos)
+            {
+             PluginManager.Manager.Server.Map.AnnounceCustomMessage("attention all facility personnel, Unauthorized personnel has been detected on the surface");
+            }
+        }
+    }
+}
